1279e3904be53ce699c89d49fdc30714192c75e0
ArenGr <arengr.1990@gmail.com>
2020-05-27 20:50:44 +0400

    all bugs fixed

--- oop/Db.php
+++ oop/Db.php
@@ -10,7 +10,6 @@ class Db extends mysqli{
     }
 
     public function select($query){
-        $query = $this->real_escape_string($query);
         $result = $this->query($query);
         $rows = [];
         if ($result) {
@@ -24,32 +23,48 @@ class Db extends mysqli{
         return $rows;
     }
 
-    public function insert($table, $data){
+    //insert without foreach
+    /* public function insert($table, $data){ */
+    /*     $keys = implode(',', array_keys($data)); */
+    /*     $values = array_values($data); */
+    /*     $cleanArray = []; */
+    /*     foreach( $values as $val ) */
+    /*             $cleanArray[] = $this->real_escape_string($val); */
+    /*     $cleanArray = implode("',' ", $cleanArray); */
+    /*     $keys = $this->real_escape_string($keys); */
+    /*     $sql_insert = "INSERT INTO $table ($keys) VALUES ('$cleanArray')"; */
+    /*     $res = $this->query($sql_insert); */
+    /*     if ($res) { */
+    /*         return true; */
+    /*     } else { */
+    /*         echo("Error description: " . $this->error); */
+    /*     } */
+    /* } */
+
+    //insert with foreach
+    public function insert($table, $data) {
         $colum = "";
         $val = "";
         foreach ($data as $key => $value) {
-            $colum .= $key.', '; 
-            $val .= "'".$value."'".', ';
+            $colum .= $this->real_escape_string($key).', '; 
+            $val .= "'".$this->real_escape_string($value)."'".', ';
         }
         $colum = substr ($colum, 0, -2);
         $val = substr ($val, 0, -2);
-        $table = $this->real_escape_string($table);
         $sql_insert = "INSERT INTO $table ($colum) VALUES ($val)";
         $res = $this->query($sql_insert);
-        if ($res===TRUE) {
+        if ($res) {
             $result = true;
         } else {
             echo("Error description: " . $this->error);
-            $result = false;
         }
-        return $result;
     }
 
     public function update($table, $data, $where){
         $colum = "";
         $val = "";
         foreach ($data as $key => $value) {
-            $val .= $key.'='."'".$value."'".', ';
+            $val .= $this->real_escape_string($key).'='."'".$this->real_escape_string($value)."'".', ';
         }
         $val = substr ($val, 0, -2);
         $sql_update = "UPDATE $table SET $val WHERE " ."$where";
@@ -57,18 +72,20 @@ class Db extends mysqli{
             return true;
         }else {
             echo("Error description: " . $this->error);
-            return false;
         }
 
     }
 
     public function delete($table, $where){
-        $sql_delete = "DELETE FROM $table WHERE " ."$where";
+        if ($where){
+            $sql_delete = "DELETE FROM $table WHERE " ."$where";
+        }else{
+            $sql_delete = "DELETE FROM $table"; 
+        }
         if ($this->query($sql_delete)) {
             return true;
         }else {
             echo("Error description: " . $this->error);
-            return false;
         }
     }
 }
@@ -78,21 +95,21 @@ $db = new Db();
 $insert_data = [
     "name" => "Jack",
     "age" => "50",
-    "country" => "Armenia",
+    "country" => "AMERICA",
     "city" => "Erevan"
 ];
+
 //insert
 /* $insert = $db->insert("customers", $insert_data);  //return boolean */
 
 //select
 /* $select = $db->select("SELECT * from customers"); */
 
-//delete
-/* $delete=$db->delete('customers', 'name="Jack"'); */
+//delete with condition
+/* $delete=$db->delete('customers', 'name= '); */
+
+//delete all
+$delete=$db->delete('customers', '');
 
 //update($table, $name, $age)// return boolean
-/* $update = $db->update('customers', $insert_data, 'age=23'); */
-
-
-
-
+/* $update = $db->update('customers', $insert_data, 'age=11'); */
