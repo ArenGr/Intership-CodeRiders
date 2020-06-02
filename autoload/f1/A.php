67bb37a23882fd55c6f85f066716b8fbf1601777
ArenGr <arengr.1990@gmail.com>
2020-06-02 08:42:35 +0400

    autoload and namespace php

---
+++ autoload/f1/A.php
@@ -0,0 +1,8 @@
+<?php
+namespace f1;
+class A {
+    function __construct(){
+        echo "class A constr";
+    }
+}
+
---
+++ autoload/index.php
@@ -0,0 +1,9 @@
+<?php
+spl_autoload_register(function($class_name){ require str_replace("\\", DIRECTORY_SEPARATOR, $class_name) .'.php';
+}
+);
+
+
+use \f2\f3\B as B;
+$obj = new B();
+
